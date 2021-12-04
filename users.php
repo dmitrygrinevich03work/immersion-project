e564225dc08efa26dc02be06bdb68a7d052dac47
dmitrygrinevich03work <dmitrygrinevich03work@gmail.com>
2021-12-04 22:00:31 +0300

    updata

--- users.php
+++ users.php
@@ -55,9 +55,9 @@ if (!is_logged_in()) {
     </nav>
 
     <main id="js-page-content" role="main" class="page-content mt-3">
-<!--        <div class="alert alert-success">-->
-<!--            Профиль успешно обновлен.-->
-<!--        </div>-->
+        <!--        <div class="alert alert-success">-->
+        <!--            Профиль успешно обновлен.-->
+        <!--        </div>-->
         <?php display_flash_message('authorization_successful', 'success'); ?>
         <div class="subheader">
             <h1 class="subheader-title">
@@ -66,7 +66,7 @@ if (!is_logged_in()) {
         </div>
         <div class="row">
             <div class="col-xl-12">
-                <?php if(is_admin()) { ?>
+                <?php if (is_admin()) { ?>
                     <a class="btn btn-success" href="create_user.html">Добавить</a>
                 <?php } ?>
                 <div class="border-faded bg-faded p-3 mb-g d-flex mt-3">
@@ -86,78 +86,106 @@ if (!is_logged_in()) {
             </div>
         </div>
         <div class="row" id="js-contacts">
-            <?php foreach ($all_user as $user) { ?>
-            <div class="col-xl-4">
-                <div id="c_1" class="card border shadow-0 mb-g shadow-sm-hover" data-filter-tags="oliver kopyov">
-                    <div class="card-body border-faded border-top-0 border-left-0 border-right-0 rounded-top">
-                        <div class="d-flex flex-row align-items-center">
+            <?php foreach ($all_user as $users) { ?>
+                <div class="col-xl-4">
+                    <div id="c_1" class="card border shadow-0 mb-g shadow-sm-hover" data-filter-tags="oliver kopyov">
+                        <div class="card-body border-faded border-top-0 border-left-0 border-right-0 rounded-top">
+                            <div class="d-flex flex-row align-items-center">
                                 <span class="status status-success mr-3">
                                     <span class="rounded-circle profile-image d-block "
                                           style="background-image:url('img/demo/avatars/avatar-b.png'); background-size: cover;"></span>
                                 </span>
-                            <div class="info-card-text flex-1">
-                                <a href="javascript:void(0);" class="fs-xl text-truncate text-truncate-lg text-info"
-                                   data-toggle="dropdown" aria-expanded="false">
-                                    <?php echo $user['user_name']; ?>
-                                    <i class="fal fas fa-cog fa-fw d-inline-block ml-1 fs-md"></i>
-                                    <i class="fal fa-angle-down d-inline-block ml-1 fs-md"></i>
-                                </a>
-                                <?php if(is_admin()) { ?>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="edit.html">
-                                        <i class="fa fa-edit"></i>
-                                        Редактировать</a>
-                                    <a class="dropdown-item" href="security.html">
-                                        <i class="fa fa-lock"></i>
-                                        Безопасность</a>
-                                    <a class="dropdown-item" href="status.html">
-                                        <i class="fa fa-sun"></i>
-                                        Установить статус</a>
-                                    <a class="dropdown-item" href="media.html">
-                                        <i class="fa fa-camera"></i>
-                                        Загрузить аватар
+                                <div class="info-card-text flex-1">
+                                    <a href="javascript:void(0);" class="fs-xl text-truncate text-truncate-lg text-info"
+                                       data-toggle="dropdown" aria-expanded="false">
+                                        <?php echo $users['user_name']; ?>
+                                        <i class="fal fas fa-cog fa-fw d-inline-block ml-1 fs-md"></i>
+                                        <i class="fal fa-angle-down d-inline-bl$userock ml-1 fs-md"></i>
                                     </a>
-                                    <a href="#" class="dropdown-item" onclick="return confirm('are you sure?');">
-                                        <i class="fa fa-window-close"></i>
-                                        Удалить
+                                    <?php
+                                    //                                $user['id'] //Получаю id авторизованого пользлователя.
+                                    //                                $users['id'] //Получаю id с цикла foreach allusers
+                                    ?>
+                                    <?php if (is_admin()) { ?>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="edit.php?id=<?php echo $users['id']; ?>">
+                                                <i class="fa fa-edit"></i>
+                                                Редактировать</a>
+                                            <a class="dropdown-item" href="security.php?id=<?php echo $users['id']; ?>">
+                                                <i class="fa fa-lock"></i>
+                                                Безопасность</a>
+                                            <a class="dropdown-item" href="status.php?id=<?php echo $users['id']; ?>">
+                                                <i class="fa fa-sun"></i>
+                                                Установить статус</a>
+                                            <a class="dropdown-item" href="media.php?id=<?php echo $users['id']; ?>">
+                                                <i class="fa fa-camera"></i>
+                                                Загрузить аватар
+                                            </a>
+                                            <a href="#" class="dropdown-item"
+                                               onclick="return confirm('are you sure?');">
+                                                <i class="fa fa-window-close"></i>
+                                                Удалить
+                                            </a>
+                                        </div>
+                                    <?php } ?>
+                                    <?php if ($user['id'] == $users['id']) { ?>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="edit.php?id=<?php echo $users['id']; ?>">
+                                                <i class="fa fa-edit"></i>
+                                                Редактировать</a>
+                                            <a class="dropdown-item" href="security.php?id=<?php echo $users['id']; ?>">
+                                                <i class="fa fa-lock"></i>
+                                                Безопасность</a>
+                                            <a class="dropdown-item" href="status.php?id=<?php echo $users['id']; ?>">
+                                                <i class="fa fa-sun"></i>
+                                                Установить статус</a>
+                                            <a class="dropdown-item" href="media.php?id=<?php echo $users['id']; ?>">
+                                                <i class="fa fa-camera"></i>
+                                                Загрузить аватар
+                                            </a>
+                                            <a href="#" class="dropdown-item"
+                                               onclick="return confirm('are you sure?');">
+                                                <i class="fa fa-window-close"></i>
+                                                Удалить
+                                            </a>
+                                        </div>
+                                    <?php } ?>
+                                    <span class="text-truncate text-truncate-xl">IT Director, Gotbootstrap Inc.</span>
+                                </div>
+                                <button class="js-expand-btn btn btn-sm btn-default d-none" data-toggle="collapse"
+                                        data-target="#c_1 > .card-body + .card-body" aria-expanded="false">
+                                    <span class="collapsed-hidden">+</span>
+                                    <span class="collapsed-reveal">-</span>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body p-0 collapse show">
+                            <div class="p-3">
+                                <a href="tel:+13174562564" class="mt-1 d-block fs-sm fw-400 text-dark">
+                                    <i class="fas fa-mobile-alt text-muted mr-2"></i> +1 317-456-2564</a>
+                                <a href="mailto:oliver.kopyov@smartadminwebapp.com"
+                                   class="mt-1 d-block fs-sm fw-400 text-dark">
+                                    <i class="fas fa-mouse-pointer text-muted mr-2"></i> <?php echo $user['email']; ?>
+                                </a>
+                                <address class="fs-sm fw-400 mt-4 text-muted">
+                                    <i class="fas fa-map-pin mr-2"></i> 15 Charist St, Detroit, MI, 48212, USA
+                                </address>
+                                <div class="d-flex flex-row">
+                                    <a href="javascript:void(0);" class="mr-2 fs-xxl" style="color:#4680C2">
+                                        <i class="fab fa-vk"></i>
+                                    </a>
+                                    <a href="javascript:void(0);" class="mr-2 fs-xxl" style="color:#38A1F3">
+                                        <i class="fab fa-telegram"></i>
+                                    </a>
+                                    <a href="javascript:void(0);" class="mr-2 fs-xxl" style="color:#E1306C">
+                                        <i class="fab fa-instagram"></i>
                                     </a>
                                 </div>
-                                <?php } ?>
-                                <span class="text-truncate text-truncate-xl">IT Director, Gotbootstrap Inc.</span>
-                            </div>
-                            <button class="js-expand-btn btn btn-sm btn-default d-none" data-toggle="collapse"
-                                    data-target="#c_1 > .card-body + .card-body" aria-expanded="false">
-                                <span class="collapsed-hidden">+</span>
-                                <span class="collapsed-reveal">-</span>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="card-body p-0 collapse show">
-                        <div class="p-3">
-                            <a href="tel:+13174562564" class="mt-1 d-block fs-sm fw-400 text-dark">
-                                <i class="fas fa-mobile-alt text-muted mr-2"></i> +1 317-456-2564</a>
-                            <a href="mailto:oliver.kopyov@smartadminwebapp.com"
-                               class="mt-1 d-block fs-sm fw-400 text-dark">
-                                <i class="fas fa-mouse-pointer text-muted mr-2"></i> <?php echo $user['email']; ?></a>
-                            <address class="fs-sm fw-400 mt-4 text-muted">
-                                <i class="fas fa-map-pin mr-2"></i> 15 Charist St, Detroit, MI, 48212, USA
-                            </address>
-                            <div class="d-flex flex-row">
-                                <a href="javascript:void(0);" class="mr-2 fs-xxl" style="color:#4680C2">
-                                    <i class="fab fa-vk"></i>
-                                </a>
-                                <a href="javascript:void(0);" class="mr-2 fs-xxl" style="color:#38A1F3">
-                                    <i class="fab fa-telegram"></i>
-                                </a>
-                                <a href="javascript:void(0);" class="mr-2 fs-xxl" style="color:#E1306C">
-                                    <i class="fab fa-instagram"></i>
-                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        <?php } ?>
+            <?php } ?>
         </div>
     </main>
 
