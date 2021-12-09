a892ff2bc2ed71119b449eb901344c5896203817
dmitrygrinevich03work <dmitrygrinevich03work@gmail.com>
2021-12-09 22:55:09 +0300

    add DB

--- DB/immersion.sql
+++ DB/immersion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Час створення: Гру 04 2021 р., 22:01
+-- Час створення: Гру 09 2021 р., 22:53
 -- Версія сервера: 8.0.15
 -- Версія PHP: 7.3.9
 
@@ -30,20 +30,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `user_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
   `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `role` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  `work` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `phone` int(11) DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `vk` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `telegram` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `instagram` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `role` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Дамп даних таблиці `users`
 --
 
-INSERT INTO `users` (`id`, `user_name`, `email`, `password`, `role`) VALUES
-(1, 'Dmityrgrinevich', 'dmitrygrinevich03@gmail.com', '$2y$10$l6dxLMdwqUTrsgVVLGU03O4AecXa.Nsyp3RqOo45f0jRY8ERxKTGK', 'admin'),
-(2, 'user', 'ewfwef@gmail.com', '$2y$10$/ZWzXK.XR9ZHIUFPjRFQ0eQ4RCqokX7lx8anzVP2.67RZKr637r/S', 'user'),
-(3, '123', '123@gmail.com', '$2y$10$8arYfY3N28hdZcGhftnMDOQMLLozyb26uL9G6zg9m4yOY9AAyzVA6', 'admin');
+INSERT INTO `users` (`id`, `user_name`, `email`, `password`, `image`, `status`, `work`, `phone`, `address`, `vk`, `telegram`, `instagram`, `role`) VALUES
+(36, '', 'dmitrygrinevich03@gmail.com', '$2y$10$P67I7Bv9/zw2YQc2IjhP2OndY0MHkJh//87CsIGs9JGAvfQpGNQ7u', '', 0, '', NULL, '', '', '', '', 'admin'),
+(38, 'Vasya', 'ewfwef@gmail.com', '$2y$10$3pZaC/C3iD3ae/srjrjr5eCnNRrUBhKonChBZjdAfbcoHlg2F4sZS', '61b25a96dfdd61com.png', 2, 'Workkkk', 988111, 'HelloAddress111', 'vk', 'tg', 'ins', 'user');
 
 --
 -- Індекси збережених таблиць
@@ -63,7 +70,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблиці `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
